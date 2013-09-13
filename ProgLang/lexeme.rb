2c6c58595b5cf5eaf6500d3ba23687a560914fb8
Joseph Sessions <Jtsessions@crimson.ua.edu>
2013-09-13 01:08:45 -0500

    Added basic data storage to Lexeme

--- ProgLang/lexeme.rb
+++ ProgLang/lexeme.rb
@@ -1,3 +1,11 @@
-class Lexeme
+class Lexeme # Class that represents a lexed token
+
+  attr_reader :type
+  attr_reader :value
+  
+  def initialize(type, value)
+    @type = type
+    @value = value
+  end
   
 end
