636c689bd78060a6a5f27e605ddf543fa484b9b0
Joseph Sessions <Jtsessions@crimson.ua.edu>
2013-09-16 13:23:53 -0500

    Adding older files
    Checkout error fix

---
+++ ProgLang/lexer.rb
@@ -0,0 +1,7 @@
+class Lexer
+  
+  def lex
+    
+  end
+  
+end
---
+++ ProgLang/main.rb
@@ -0,0 +1,11 @@
+class Main
+  
+  def initialize
+    dog = gets
+    endfileName = gets("Please supply an absolute file path for your source code.") #Interpreter doesnt seem to like gets
+    p "kbesgkjbesgljbdsfkjfsabljbfsa"
+  end
+  
+end
+
+m = Main.new
