2171a5e75a5611eab35964d497cd7ea36e1eba0c
Joseph <Joseph@Joseph-PC>
2013-09-16 13:03:48 -0500

    Adding basic string/integer recognition

--- ProgLang/lexer.rb
+++ ProgLang/lexer.rb
@@ -3,7 +3,7 @@ require 'strscan'
 class Lexer # Class that creates lexemes for each symbol encountered
   
   def initialize( filePath )
-    @text = "        define cherry ( chicken sandwich ) { chicken / sandwich }" # This is a placeholder. Grab the file instead!
+    @text = "        define cherry ( chicken sandwich ) { chicken / 42 + \"Cake\" }" # This is a placeholder. Grab the file instead!
     @scanner = StringScanner.new( @text )
     lex
   end
@@ -12,48 +12,67 @@ class Lexer # Class that creates lexemes for each symbol encountered
     
     results = [] # Create an array to store the resulting chain of lexemes for return
     
-    nextToken = @scanner.scan(/\s*(\w+|[(){}+\-*\/])/) # Match optional whitespace, then a word or parenthesis/bracket
+    nextToken = @scanner.scan(/\s*(\w+|[(){}+\-*\/"'])/) # Match optional whitespace, then a word or parenthesis/bracket
     p nextToken.strip
     until nextToken == nil
     
       type = nil # Initialize type and value fields so that we can factor out the lexeme constructor statement
       value = nil
     
-      case nextToken.strip
-        when "define"
+      case nextToken.strip.downcase
+        when 'define'
           type = :DEFINE
-        when "("
+        when '('
           type = :OPAREN
-        when ")"
+        when ')'
           type = :CPAREN
-        when "{"
+        when '{'
           type = :OBRACKET
-        when "}"
+        when '}'
           type = :CBRACKET
-        when "/"
+        when '/'
           type = :FSLASH
-        when "\\"
+        when '\\'
           type = :BSLASH
-        when "+"
+        when '+'
           type = :PLUS
-        when "-"
+        when '++'
+          type = :PLUSPLUS
+        when '-'
           type = :MINUS
-        when ";"
+        when '--'
+          type = :MINUSMINUS
+        when ';'
           type = :SEMICOLON
-        when ":"
+        when ':'
           type = :COLON
-        when "if"
+        when 'if'
           type = :IF
-        when "else"
+        when 'else'
           type = :ELSE
         else # For now, instead of "token not implemented", we'll say they're identifiers.
-          type = :IDENTIFIER
-          value = nextToken.strip
+          if nextToken.match(/\d+/)
+            type = :INTEGER
+            value = nextToken.strip
+          elsif nextToken.strip == '"' # Instead, when you find an open quote, take the next token until
+            type = :DSTRING
+            nextToken = @scanner.scan(/.*"/)[0]
+            value = nextToken
+          elsif nextToken.strip == "'" # We find a close quote. If we don't, error!
+            type = :SSTRING
+            nextToken = @scanner.scan(/.*'/)[0]
+            value = nextToken
+          else
+            type = :IDENTIFIER
+            value = nextToken.strip
+          end
       end
       
       results.push( Lexeme.new( type, value ))
     
-      nextToken = @scanner.scan(/\s+(\w+|[(){}+\-*\/])/) # Match whitespace, then a word or parenthesis/bracket
+      nextToken = @scanner.scan(/\s+(\w+|[(){}+\-*\/"'])/) # Match whitespace, then a word or parenthesis/bracket
+      
+      p nextToken
     end
 
     results.each { |x| print "#{x}\n" }
