c1d3bc6d664f6bd9cb9e4a2f8204dd559356bff0
Joseph Sessions <Jtsessions@crimson.ua.edu>
2013-09-17 09:55:10 -0500

    Adding string lexeme recognition
    Finalized the string lexeme creation code. Strings are now properly
    recognized.

--- ProgLang/lexer.rb
+++ ProgLang/lexer.rb
@@ -3,7 +3,7 @@ require 'strscan'
 class Lexer # Class that creates lexemes for each symbol encountered
   
   def initialize( filePath )
-    @text = "        define cherry ( chicken sandwich ) { chicken / 42 + \"Cake\" }" # This is a placeholder. Grab the file instead!
+    @text = "   define define define cakemix _dog 849894usi jahd@      define cherry ( chicken sandwich ) { 5chicken + chicken / 4255ff - 42 + \"Cake\" }" # This is a placeholder. Grab the file instead!
     @scanner = StringScanner.new( @text )
     lex
   end
@@ -12,7 +12,7 @@ class Lexer # Class that creates lexemes for each symbol encountered
     
     results = [] # Create an array to store the resulting chain of lexemes for return
     
-    nextToken = @scanner.scan(/\s*(\w+|[(){}+\-*\/"'])/) # Match optional whitespace, then a word or parenthesis/bracket
+    nextToken = @scanner.scan(/\s*(\w+|[(){}+\-*\/"'\@])/) # Match optional whitespace, then a word or parenthesis/bracket
     p nextToken.strip
     until nextToken == nil
     
@@ -51,20 +51,23 @@ class Lexer # Class that creates lexemes for each symbol encountered
         when 'else'
           type = :ELSE
         else # For now, instead of "token not implemented", we'll say they're identifiers.
-          if nextToken.match(/\d+/)
+          if nextToken.strip.match(/^\d+$/)
             type = :INTEGER
             value = nextToken.strip
           elsif nextToken.strip == '"' # Instead, when you find an open quote, take the next token until
             type = :DSTRING
-            nextToken = @scanner.scan(/.*"/)[0]
-            value = nextToken
+            nextToken = @scanner.scan(/.*"/)
+            value = nextToken[0...-1] # We don't wanna keep that last quotation mark
           elsif nextToken.strip == "'" # We find a close quote. If we don't, error!
             type = :SSTRING
-            nextToken = @scanner.scan(/.*'/)[0]
-            value = nextToken
-          else
+            nextToken = @scanner.scan(/.*'/)
+            value = nextToken[0...-1] # We don't wanna keep that last quotation mark
+          elsif nextToken.strip.match(/^[a-zA-Z]\w*$/) # Match any identifier starting with a letter
             type = :IDENTIFIER
             value = nextToken.strip
+          else
+            type = :INVALIDTOKEN
+            value = "!!!INVALID: |#{nextToken}|!!!"
           end
       end
       
